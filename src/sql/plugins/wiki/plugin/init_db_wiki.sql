f7b01bcd766b93c9fedb6244402342e653cf29d0
pierrelevy <pierrelevy@users.noreply.github.com>
2013-04-02 14:52:55 +0000

    WIKI-10 : Data Sample - SQL initialization
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/collaboration/plugin-wiki@47586 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/sql/plugins/wiki/plugin/init_db_wiki.sql
@@ -0,0 +1,13 @@
+
+--
+-- Dumping data for table `wiki_topic`
+--
+
+INSERT INTO `wiki_topic` VALUES (1,0,'Accueil');
+
+--
+-- Dumping data for table `wiki_topic_version`
+--
+
+INSERT INTO `wiki_topic_version` 
+VALUES (1,'',1,'admin','2013-04-02 14:39:36',0,'===Bienvenue dans le Wiki de LUTECE===\r\n\r\nCe plugin vous permet d\'ajouter à votre site Lutece les fonctionnalités d\'un Wiki.\r\n\r\nIl vous permet de créer de manière très simple des pages grâce à un éditeur de syntaxe Wiki et\r\nune aide facilement accessible.\r\n\r\nPour créer une nouvelle page, il suffit de créer le lien vers cette page dans une page existante, de cliquer sur ce lien et éditer la page vierge affichée.\r\n\r\n=== Autres ressources ===\r\n\r\nVoici la liste des sites LUTECE :\r\n\r\n|=URL|=Description|\r\n|[[http://fr.lutece.paris.fr|http://fr.luteceparis.fr]]|Site officiel de Lutece|\r\n|[[http://dev.lutece.paris.fr/site-demo|http://dev.luteceparis.fr/site-demo]]|Site de démonstration|\r\n|[[http://dev.lutece.paris.fr/fr|http://dev.luteceparis.fr/fr]]|Site de développement Open Source|\r\n\r\n');
