e2f9cf60eecfb758da3a6b02ccf3a2cb7e767895
pierrelevy <pierrelevy@users.noreply.github.com>
2014-05-16 21:33:44 +0000

    WIKI-47 : Let define Wiki's home page using site's properties
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/collaboration/plugin-wiki@66533 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/sql/plugins/wiki/core/init_db_core_wiki.sql
@@ -0,0 +1,7 @@
+--
+-- Dumping data for table core_datastore
+--
+REPLACE INTO `core_datastore` VALUES 
+('wiki.site_property.path.rootLabel','Accueil du Wiki'),
+('wiki.site_property.path.rootPageName','accueil');
+
