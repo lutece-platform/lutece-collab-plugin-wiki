607b02a17d57ad4d0b8c3ba0ee93e33a03658bfb
pierrelevy <pierrelevy@users.noreply.github.com>
2014-05-29 15:45:42 +0000

    WIKI-49 : Let define a Prettify skin into Site's properties
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/collaboration/plugin-wiki@68466 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/wiki/core/init_db_core_wiki.sql
+++ src/sql/plugins/wiki/core/init_db_core_wiki.sql
@@ -4,7 +4,8 @@
 REPLACE INTO core_datastore VALUES 
 ('wiki.site_property.path.rootLabel','Accueil du Wiki'),
 ('wiki.site_property.path.rootPageName','accueil'),
-('wiki.site_property.role.admin','wiki_admin');
+('wiki.site_property.role.admin','wiki_admin'),
+('wiki.site_property.prettify.skin','sunburst');
 
 INSERT INTO core_role VALUES 
 ( 'wiki_admin' , 'Wiki Administrator (can remove content)', '' ),
