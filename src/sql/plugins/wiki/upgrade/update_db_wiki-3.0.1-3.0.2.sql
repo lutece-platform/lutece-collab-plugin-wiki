fd91ccbbf652997d45c1388688823fc32187b06c
timothee-hx <timothee.harleaux@atos.net>
2023-05-09 14:38:15 +0200

    Added is_published column in wiki_topic_version in the upgrade sql file

--- src/sql/plugins/wiki/upgrade/update_db_wiki-3.0.1-3.0.2.sql
+++ src/sql/plugins/wiki/upgrade/update_db_wiki-3.0.1-3.0.2.sql
@@ -1,1 +1,3 @@
 ALTER TABLE wiki_topic ADD COLUMN parent_page_name VARCHAR(100) DEFAULT '' NOT NULL;
+ALTER TABLE wiki_topic_version ADD COLUMN is_published INT DEFAULT '0' NOT NULL;
+UPDATE wiki_topic_version wtv1 SET is_published = 1 WHERE id_topic_version = (SELECT MAX(id_topic_version) FROM wiki_topic_version wtv2 WHERE wtv1.id_topic = wtv2.id_topic);
