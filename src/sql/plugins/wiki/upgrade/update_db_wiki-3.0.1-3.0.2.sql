b32ae314b3a3709a4b2d757d77a7c0e71bfd6eeb
TimotheeHrl <tharleaux@gmail.com>
2024-10-01 13:50:20 +0200

    LUT-25169: create an option draft to save the work in progress

--- src/sql/plugins/wiki/upgrade/update_db_wiki-3.0.1-3.0.2.sql
+++ src/sql/plugins/wiki/upgrade/update_db_wiki-3.0.1-3.0.2.sql
@@ -1,1 +1,15 @@
 ALTER TABLE wiki_topic ADD COLUMN parent_page_name VARCHAR(100) DEFAULT '' NOT NULL;
+ALTER TABLE wiki_topic_version ADD COLUMN is_published INT DEFAULT '0' NOT NULL;
+
+--
+-- for each id_topic, set is_published to 1 for the latest version
+--
+UPDATE wiki_topic_version
+    JOIN (
+        SELECT id_topic, MAX(id_topic_version) AS max_id_topic_version
+        FROM wiki_topic_version
+        GROUP BY id_topic
+    ) AS max_versions ON wiki_topic_version.id_topic = max_versions.id_topic
+        AND wiki_topic_version.id_topic_version = max_versions.max_id_topic_version
+SET wiki_topic_version.is_published = 1;
+
